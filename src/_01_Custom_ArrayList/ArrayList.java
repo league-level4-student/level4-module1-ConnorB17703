52b40267f239d09052ec853e2342e8d3f7060459
cj3an <cj3an@DESKTOP-3LMP021>
2021-02-22 18:01:19 -0800

    ArrayList.java, SwitchStatementDemo.java, CustonButtonOptionPanes.java, IntoToEnums.java

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -27,20 +27,59 @@ public class ArrayList <T>{
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
 		
-		//You left off on this method, FINISH IT
+		T[] newArr = (T[]) new Object[memberArr.length+1];
+		
+		for(int i = 0; i < newArr.length; i++){
+			if(i< loc){
+				newArr[i] = memberArr[i];
+			}
+			else if(i>loc){
+				newArr[i] = memberArr[i-1];
+			}
+			else{
+				newArr[i] = val;
+			}
+		}
+		memberArr = newArr;
 		
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		memberArr[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+
+		T[] newArr = (T[]) new Object[memberArr.length-1];
 		
+		for(int i = 0; i < newArr.length; i++){
+			if(i< loc){
+				newArr[i] = memberArr[i];
+			}
+			else if(i>loc){
+				newArr[i-1] = memberArr[i];
+			}
+			else{
+				continue;
+			}
+		}
+		memberArr = newArr;
 	}
 	
 	public boolean contains(T val) {
 		
+		for(int i = 0; i<memberArr.length;i++){
+			if(memberArr[i] == val){
+				return true;
+			}
+		}
 		return false;
 	}
+	
+	
+	public int size(){
+		return memberArr.length;
+		
+	}
+	
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -11,7 +11,35 @@ public class CustomButtonOptionPanes {
 
 		String choice = options[input];
 		
-		//use a switch statement to do something cool for each option
-		
+		switch(choice){
+		case "Sunday":{
+			System.out.println("time to relax!");
+			break;
+		}
+		case "Monday":{
+			System.out.println("UUUGGGHHHHH");
+			break;
+		}
+		case "Tuesday":{
+			System.out.println("2 more days to go!");
+			break;
+		}
+		case "Wednesday":{
+			System.out.println("WOOOOOOO Wednesday");
+			break;
+		}
+		case "Thursday":{
+			System.out.println("Almost friday!!!");
+			break;
+		}
+		case "Friday":{
+			System.out.println("YYYYYEEEESSSSS");
+			break;
+		}
+		case "Saturday":{
+			System.out.println("THE WEEKEND!!!");
+			break;
+		}
+		}
 	}
 }
